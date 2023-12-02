3c4eb890e64a7f008425606d363c6929af79cb40
Wvaviator <wvaviator@gmail.com>
2023-12-02 10:49:11 -0600

    quick test refactor

--- src/challenge/cube_conundrum/mod.rs
+++ src/challenge/cube_conundrum/mod.rs
@@ -53,9 +53,8 @@ impl CubeConundrum {
 mod test {
     use super::*;
 
-    #[test]
-    fn ch02_cubeconundrum_part_one() {
-        let test_lines = vec![
+    fn get_mock_input() -> Vec<String> {
+        vec![
             String::from("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"),
             String::from("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"),
             String::from(
@@ -65,7 +64,12 @@ mod test {
                 "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
             ),
             String::from("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"),
-        ];
+        ]
+    }
+
+    #[test]
+    fn ch02_cubeconundrum_part_one() {
+        let test_lines = get_mock_input();
 
         let mut cube_conundrum = CubeConundrum::default();
         cube_conundrum.load_lines(test_lines);
@@ -75,17 +79,7 @@ mod test {
 
     #[test]
     fn ch02_cubeconundrum_part_two() {
-        let test_lines = vec![
-            String::from("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"),
-            String::from("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"),
-            String::from(
-                "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
-            ),
-            String::from(
-                "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
-            ),
-            String::from("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"),
-        ];
+        let test_lines = get_mock_input();
 
         let mut cube_conundrum = CubeConundrum::default();
         cube_conundrum.load_lines(test_lines);
